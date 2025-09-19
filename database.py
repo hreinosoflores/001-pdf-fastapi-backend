d529254200774732315a438fd908a4e4d8efd284
Humberto Reinoso <hreinosoflores@gmail.com>
2025-09-18 19:01:55 -0500

    first commit

---
+++ database.py
@@ -0,0 +1,22 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+
+load_dotenv()
+
+user = os.environ['DATABASE_USER']
+password = os.environ['DATABASE_PASSWORD']
+host = os.environ['DATABASE_HOST']
+port = os.environ['DATABASE_PORT']
+db_name = os.environ['DATABASE_NAME']
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{user}:{password}@{host}:{port}/{db_name}"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
